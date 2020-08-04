d4ec61e61dea58bdf47d231c3467bc8655cf457c
Simen Pjaaten <simenpjaat@gmail.com>
2020-08-04 11:55:08 +0200

    Added class to handle API interaction

---
+++ JokeApp/APIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JokeApp
+{
+    class APIController
+    {
+        // A class to control our Joke API
+        // Define some properties of our class
+        public string APIURL;
+
+        // Create constructor to set variables
+        public APIController()
+        {
+            // Let's put the URL in our environmental variables to make it easier to update it later if needed
+            this.APIURL = System.Environment.GetEnvironmentVariable("RandomJokeAPIURL", EnvironmentVariableTarget.Process);
+        }
+    }
+}
